66e2aa8925bc4acf6565977ece936cd7ff22e712
The Arcterion Project <shaynemganness@gmail.com>
2022-01-14 18:29:47 -0500

    switch app.get to json

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/api/notes', (req, res) => {
-  res.send('Hello!');
+  res.json(notes);
 });
 app.listen(3001, () => {
     console.log(`API server now on port 3001!`);
