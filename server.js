d208920004970901f5c57fd415fe277961e2db2f
The Arcterion Project <shaynemganness@gmail.com>
2022-01-14 18:28:06 -0500

    test route

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
+const { notes } = require('./db/db');
 const express = require('express');
 const app = express();
 
+app.get('/api/notes', (req, res) => {
+  res.send('Hello!');
+});
 app.listen(3001, () => {
     console.log(`API server now on port 3001!`);
   });
