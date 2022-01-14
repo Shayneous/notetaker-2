4d4b32f614e1e4d4edab44da3708142c34588ec1
The Arcterion Project <shaynemganness@gmail.com>
2022-01-14 18:42:54 -0500

    const PORT modified for heroku

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const PORT = process.env.PORT || 3001;
 const { notes } = require('./db/db');
 const express = require('express');
 const app = express();
@@ -5,6 +6,6 @@ const app = express();
 app.get('/api/notes', (req, res) => {
   res.json(notes);
 });
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
-  });
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
+});
